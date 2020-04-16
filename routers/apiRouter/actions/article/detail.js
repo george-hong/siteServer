1b8a36113e83165c56d7900c797d85fe5d4fd130
a916856595 <hcj147@vip.qq.com>
2020-04-17 02:02:17 +0800

    1.增加查询文章详情接口

---
+++ routers/apiRouter/actions/article/detail.js
@@ -0,0 +1,23 @@
+import mySQL from '../../../../mySQL/index';
+import { tableNames, requestParamsField, responseContainerField } from '../../../../mySQL/config';
+import { extractFieldsAsAObject } from '../../../utilities/serverUtilities';
+
+const queryArticle = async(request, response, next) => {
+  const { [requestParamsField]: requestParams } = request;
+  const { [responseContainerField]: responseContainer } = response;
+  const fields = ['id'];
+  const queryObject = extractFieldsAsAObject(requestParams, fields);
+  try {
+    const [queryResult] = await mySQL.queryItem(tableNames.article, {
+      fields: queryObject,
+      limit: 1,
+    }, '*');
+    responseContainer.status = 200;
+    responseContainer.data = queryResult;
+  } catch (err) {
+    responseContainer.data = err;
+  }
+  next();
+};
+
+export default queryArticle;
