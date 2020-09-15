1f6207e37c879b25182a53327706c04f6d3ea3f0
a916856595 <hcj147@vip.qq.com>
2020-09-15 09:40:10 +0800

    修改文件上传保存目录的配置

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const config = {
-    uploadFileRootFolder: '/app/upload',            // 上传文件的目录
+    localSaveUploadFileRootFolder: '/app/upload',         // 本机保存上传文件在项目根目录中的位置
+    serverReadUploadFileRootFolder: '/upload',            // 服务器读取上传文件在app目录中的位置
 };
 
 export default config;
