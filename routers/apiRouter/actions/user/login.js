9e7bee565d3cf5441d779bfbdef2e997bcee139c
a916856595 <hcj147@vip.qq.com>
2020-09-20 15:56:54 +0800

    24.添加登录密码校验

--- routers/apiRouter/actions/user/login.js
+++ routers/apiRouter/actions/user/login.js
@@ -1,20 +1,23 @@
 import mySQL from '../../../../mySQL/index';
 import { createTokenInfo } from '../../../utilities/tokenUtilities';
 import { tableNames, requestParamsField, responseContainerField } from '../../../../mySQL/config';
+import {extractFieldsAsAObject} from "../../../utilities/serverUtilities";
 
 // 如果查询到则返回账号相关字段，否则返回Null
 const checkAccountIsRepeat = async(request, response, next) => {
   const { [requestParamsField]: requestParams } = request;
   const { [responseContainerField]: responseContainer } = response;
-  const accountField = 'account';
+  const fields = ['account', 'password'];
+  const queryObject = extractFieldsAsAObject(requestParams, fields);
   try {
-    const [queryResult] = await mySQL.queryItem(tableNames.user, {
-      fields: { [accountField]: requestParams[accountField] },
+    const { account, password } = queryObject;
+    let [queryResult] = await mySQL.queryItem(tableNames.user, {
+      fields: { account },
       limit: 1,
-    }, 'id,account,userName,avatar,backgroundImage,introduction');
+    }, '*');
     // 登录成功后生成jwt
     let token = null;
-    if (queryResult) {
+    if (queryResult && queryResult.password === password) {
       const { account, userName } = queryResult;
       const payload = {
         account,
@@ -34,7 +37,8 @@ const checkAccountIsRepeat = async(request, response, next) => {
       await mySQL.insert(tableNames.token, tokenInfo);
       queryResult.userId = queryResult.id;
       delete queryResult.id;
-    }
+      delete queryResult.password;
+    } else queryResult = null;
     responseContainer.status = 200;
     responseContainer.data = {
       accountInfo: queryResult || null,
